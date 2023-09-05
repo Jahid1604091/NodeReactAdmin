f23828fbb62f71c45932c562384008912e654a55
jahid1604091 <jh409780@gmail.com>
2023-09-05 15:02:33 +0600

    test route added

--- index.js
+++ index.js
@@ -18,7 +18,9 @@ const app = express();
 app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
-app.use(morgan("common"));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("common"));
+}
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
@@ -28,5 +30,7 @@ app.use("/client", clientRoute);
 app.use("/general", generalRoute);
 app.use("/management", managementRoute);
 app.use("/sales", salesRoute);
+
+app.get("/", (req, res) => res.send("<h1> Server is UP </h1>"));
 
 app.listen(PORT, console.log("Server run on port " + PORT));
